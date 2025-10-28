260dcee7bd56b20d79149bda25c0300c2d29f84d
Mostafa <mostafashokrymohamed33@gmail.com>
2025-10-28 19:10:39 +0300

    mosatafa:HOMe stage 1

--- app/page.js
+++ app/page.js
@@ -1,12 +1,49 @@
+// "use client";
 import Image from "next/image";
 import Sidebar from "./components/sidebar/sidebar";
+import ProductCard from "./components/ProductCard";
+// import { useEffect, useState } from "react";
 export default function Home() {
+  // const [products, setProducts] = useState([]);
+  // useEffect(()=>{
+  //   fetch('https://api.escuelajs.co/api/v1/products')
+  //   .then(res=>res.json())
+  //   .then(data=>{
+  //     setProducts(data);
+  //     console.log(data);
+  //   })
+  //   .catch(err=>alert('Error fetching products'));
+  // },[])
   return (
     <div className="flex ">
       <Sidebar/>
-      <main className="flex-1 p-8">
-        <h1 className="text-2xl font-bold caret-amber-900 bg-blue-800 text-center">يامصطفى دا الهوم اشتغل هون </h1>
-
+      <main className="mostafa-task">
+        <div className="search-filters">
+        </div>
+        <div className="products">
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+        </div>
       </main>
     </div>
   );
